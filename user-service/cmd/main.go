fc74d0d46a1568c68979dc61f02f9e57ba1832f0
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-05-07 18:21:43 -0300

    feat: adding connection file for pgx

--- user-service/cmd/main.go
+++ user-service/cmd/main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log"
+
+	"github.com/FelipeFelipeRenan/goverse/user-service/pkg/database"
+)
+
 func main() {
+
+	conn, err := database.Connect()
+	if err != nil {
+		log.Fatalf("Erro ao conectar com banco de dados: %v", err)
+	}
+	defer conn.Close(nil)
+
+	
 	
 }
---
+++ user-service/pkg/database/pgx.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
+)
+
+func LoadEnv() error{
+	err := godotenv.Load("./user-service/.env")
+	if err != nil {
+		return fmt.Errorf("Erro ao carregar configurações do banco de dados: %v", err)
+	}
+	return nil
+}
+
+
+func Connect() (*pgx.Conn, error){
+
+	err := LoadEnv()
+	if err != nil {
+		return nil, err
+	}
+
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", dbUser, dbPassword, dbHost, dbPort, dbName)
+	conn, err := pgx.Connect(nil, connStr)
+	if err != nil {
+		return nil, fmt.Errorf("Não foi possível conectar ao banco de dados: %v", err)
+	}
+
+	return conn, nil
+}
