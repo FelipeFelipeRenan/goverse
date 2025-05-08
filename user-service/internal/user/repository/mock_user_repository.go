71ae0b34d52c88f9c108dcb2b2129e030c14b1c9
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-05-08 18:55:58 -0300

    feat: adding mock to user repository

---
+++ user-service/internal/user/repository/mock_user_repository.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"context"
+
+	"github.com/FelipeFelipeRenan/goverse/user-service/internal/user/domain"
+	"github.com/stretchr/testify/mock"
+)
+
+type MockUserRepository struct {
+	mock.Mock
+}
+
+
+func (m *MockUserRepository) CreateUser(ctx context.Context, user domain.User) (string, error){
+	args := m.Called(ctx, user)
+	return args.String(0), args.Error(1)
+}
+
+func (m *MockUserRepository) GetUserByID(ctx context.Context, id string)(*domain.User, error){
+	args := m.Called(ctx, id)
+	if args.Get(0) == nil{
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*domain.User), args.Error(1)
+}
