9c5c4a17de526a8c16d00d4c5afbc998c068505e
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-05-09 17:30:07 -0300

    test: adding tests to user service

---
+++ user-service/internal/user/service/user_service_test.go
@@ -0,0 +1,76 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"testing"
+
+	"github.com/FelipeFelipeRenan/goverse/user-service/internal/user/domain"
+	"github.com/FelipeFelipeRenan/goverse/user-service/internal/user/repository"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+
+func TestUserService_Register_Success(t *testing.T) {
+	mockRepo := new(repository.MockUserRepository)
+	svc := NewUserService(mockRepo)
+
+	user := domain.User{
+		Username: "alice",
+		Email:    "alice@example.com",
+		Password: "securepass",
+	}
+
+	mockRepo.On("CreateUser", mock.Anything, mock.AnythingOfType("domain.User")).Return("123", nil)
+
+	id, err := svc.Register(context.Background(), user)
+
+	assert.NoError(t, err)
+	assert.Equal(t, "123", id)
+	mockRepo.AssertExpectations(t)
+}
+
+func TestUserService_Register_InvalidData(t *testing.T) {
+	mockRepo := new(repository.MockUserRepository)
+	svc := NewUserService(mockRepo)
+
+	user := domain.User{} // Dados vazios
+
+	id, err := svc.Register(context.Background(), user)
+
+	assert.Error(t, err)
+	assert.Equal(t, "", id)
+}
+
+func TestUserService_FindByID_Success(t *testing.T) {
+	mockRepo := new(repository.MockUserRepository)
+	svc := NewUserService(mockRepo)
+
+	expected := &domain.User{
+		ID:       "123",
+		Username: "bob",
+		Email:    "bob@example.com",
+	}
+
+	mockRepo.On("GetUserByID", mock.Anything, "123").Return(expected, nil)
+
+	user, err := svc.FindByID(context.Background(), "123")
+
+	assert.NoError(t, err)
+	assert.Equal(t, expected, user)
+	mockRepo.AssertExpectations(t)
+}
+
+func TestUserService_FindByID_NotFound(t *testing.T) {
+	mockRepo := new(repository.MockUserRepository)
+	svc := NewUserService(mockRepo)
+
+	mockRepo.On("GetUserByID", mock.Anything, "not_found").Return(nil, errors.New("not found"))
+
+	user, err := svc.FindByID(context.Background(), "not_found")
+
+	assert.Error(t, err)
+	assert.Nil(t, user)
+	mockRepo.AssertExpectations(t)
+}
