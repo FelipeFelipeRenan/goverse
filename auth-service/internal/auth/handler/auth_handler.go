36517228b793cee13461d94759499127bf7fd6ac
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-05-12 18:47:03 -0300

    feat: adding auth handler

--- auth-service/internal/auth/handler/auth_handler.go
+++ auth-service/internal/auth/handler/auth_handler.go
@@ -1,1 +1,35 @@
 package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/FelipeFelipeRenan/goverse/auth-service/internal/auth/domain"
+	"github.com/FelipeFelipeRenan/goverse/auth-service/internal/auth/service"
+)
+
+type AuthHandler struct {
+	authService service.AuthService
+}
+
+func NewAuthService(authService service.AuthService) *AuthHandler{
+	return &AuthHandler{authService: authService}
+}
+
+func (h *AuthHandler) Login(w http.ResponseWriter, r *http.Request){
+	var creds domain.Credentials
+
+	if err := json.NewDecoder(r.Body).Decode(&creds); err != nil{
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	tokenResp, err := h.authService.Authenticate(r.Context(), creds)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusUnauthorized)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(tokenResp)
+}
