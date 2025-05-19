8ce9e4b99bb9082a4c00ff113d67dd867f7b23c6
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-05-19 19:15:22 -0300

    fix: fixing api gateway redirection to user service creation and return by id methods

--- api-gateway/cmd/main.go
+++ api-gateway/cmd/main.go
@@ -10,23 +10,30 @@ import (
 	"github.com/FelipeFelipeRenan/goverse/api-gateway/pkg/logger"
 )
 
-
 func main() {
 
 	logger.Init()
 	mux := http.NewServeMux()
 
-	mux.Handle("/auth", middleware.LoggingMiddleware(proxy.NewReverseProxy("http://auth-service:8081")))
+	// Rotas do auth service
+	mux.Handle("/login", middleware.LoggingMiddleware(proxy.NewReverseProxy("http://auth-service:8081")))
+	mux.Handle("/oauth/google/login", middleware.LoggingMiddleware(proxy.NewReverseProxy("http://auth-service:8081")))
+	mux.Handle("/oauth/google/callback", middleware.LoggingMiddleware(proxy.NewReverseProxy("http://auth-service:8081")))
 
+	// Rotas do user-service
 	mux.Handle("/users", middleware.LoggingMiddleware(proxy.NewReverseProxy("http://user-service:8080")))
+	// Criar usuario
+	mux.Handle("/user", middleware.LoggingMiddleware(proxy.NewReverseProxy("http://user-service:8080")))
+	// retornar usuario por id
+	mux.Handle("/user/", middleware.LoggingMiddleware(proxy.NewReverseProxy("http://user-service:8080")))
 
 	port := os.Getenv("GATEWAY_PORT")
-	if port == ""{
+	if port == "" {
 		port = "8080"
 	}
 
 	logger.Info.Info("API Gateway rodando", "port", port)
-	if err := http.ListenAndServe(":"+port, mux); err != nil{
+	if err := http.ListenAndServe(":"+port, mux); err != nil {
 		log.Fatalf("Erro ao iniciar API Gateway: %v", err)
 	}
 }
