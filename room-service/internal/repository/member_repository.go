ebce933b9f347f5e8517ec23f9fed26b629997a0
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-05-29 17:23:45 -0300

    chore: adding update member role at room member repository

--- room-service/internal/repository/member_repository.go
+++ room-service/internal/repository/member_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/FelipeFelipeRenan/goverse/room-service/internal/domain"
@@ -11,10 +12,11 @@ import (
 
 type RoomMemberRepository interface {
 	AddMember(ctx context.Context, member *domain.RoomMember) error
-	RemoveMember(roomID, userID string) error
-	GetMembers(roomID string) ([]*domain.RoomMember, error)
+	RemoveMember(ctx context.Context, roomID, userID string) error
+	GetMembers(ctx context.Context, roomID string) ([]*domain.RoomMember, error)
 	GetMemberByID(ctx context.Context, roomID, userID string) (*domain.RoomMember, error)
-	GetUserRole(roomID, userID string) (domain.Role, error)
+	GetUserRole(ctx context.Context, roomID, userID string) (domain.Role, error)
+	UpdateMemberRole(ctx context.Context, roomID, userID string, newRole domain.Role) error
 	IsMember(ctx context.Context, roomID, userID string) (bool, error)
 }
 
@@ -48,9 +50,9 @@ func (r *roomMemberRepository) AddMember(ctx context.Context, member *domain.Roo
 }
 
 // RemoveMember implements RoomMemberRepository.
-func (r *roomMemberRepository) RemoveMember(roomID string, userID string) error {
+func (r *roomMemberRepository) RemoveMember(ctx context.Context, roomID string, userID string) error {
 	query := `DELETE FROM room_members WHERE room_id = $1 AND user_id = $2`
-	_, err := r.db.Exec(context.Background(), query, roomID, userID)
+	_, err := r.db.Exec(ctx, query, roomID, userID)
 	return err
 }
 
@@ -76,14 +78,14 @@ func (r *roomMemberRepository) GetMemberByID(ctx context.Context, roomID string,
 }
 
 // GetMembers implements RoomMemberRepository.
-func (r *roomMemberRepository) GetMembers(roomID string) ([]*domain.RoomMember, error) {
+func (r *roomMemberRepository) GetMembers(ctx context.Context, roomID string) ([]*domain.RoomMember, error) {
 	query := `
 		SELECT user_id, role, joined_at
 		FROM room_members
 		WHERE room_id = $1
 	`
 
-	rows, err := r.db.Query(context.Background(), query, roomID)
+	rows, err := r.db.Query(ctx, query, roomID)
 	if err != nil {
 		return nil, err
 	}
@@ -106,11 +108,11 @@ func (r *roomMemberRepository) GetMembers(roomID string) ([]*domain.RoomMember, 
 }
 
 // GetUserRole implements RoomMemberRepository.
-func (r *roomMemberRepository) GetUserRole(roomID string, userID string) (domain.Role, error) {
+func (r *roomMemberRepository) GetUserRole(ctx context.Context, roomID string, userID string) (domain.Role, error) {
 	query := `SELECT role FROM room_members WHERE room_id = $1 AND user_id = $2`
 
 	var roleStr string
-	err := r.db.QueryRow(context.Background(), query, roomID, userID).Scan(roleStr)
+	err := r.db.QueryRow(ctx, query, roomID, userID).Scan(roleStr)
 	if err != nil {
 		if errors.Is(err, pgx.ErrNoRows) {
 			return "", nil
@@ -118,6 +120,24 @@ func (r *roomMemberRepository) GetUserRole(roomID string, userID string) (domain
 		return "", err
 	}
 	return domain.Role(roleStr), nil
+}
+
+// UpdateMemberRole implements RoomMemberRepository.
+func (r *roomMemberRepository) UpdateMemberRole(ctx context.Context, roomID string, userID string, newRole domain.Role) error {
+	query := `
+		UPDATE room_members
+		SET role = $1
+		WHERE room_id = $2 AND user_id = $3
+	`
+
+	cmdTag, err := r.db.Exec(ctx, query, newRole, roomID, userID)
+	if err != nil {
+		return err
+	}
+	if cmdTag.RowsAffected() == 0 {
+		return fmt.Errorf("nenhum membro encontrado com room_id=%s e user_id=%s", userID, roomID)
+	}
+	return nil
 }
 
 // IsMember implements RoomMemberRepository.
