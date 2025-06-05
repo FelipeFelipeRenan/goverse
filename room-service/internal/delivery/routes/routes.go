f3c6ce4e319f9babe9f1d209ce2673a957fde4e8
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-06-05 18:18:16 -0300

    feat: adding add member, delete member, list room members and update member role methods to member handler

--- room-service/internal/delivery/routes/routes.go
+++ room-service/internal/delivery/routes/routes.go
@@ -7,12 +7,19 @@ import (
 	"github.com/FelipeFelipeRenan/goverse/room-service/middleware"
 )
 
-func RegisterRoutes(roomHandler *handler.RoomHandler) {
+func RegisterRoutes(roomHandler *handler.RoomHandler, memberHandler *handler.MemberHandler) {
 
+	// Rotas de rooms
 	http.HandleFunc("POST /rooms", middleware.LoggingMiddleware(roomHandler.CreateRoom))
 	http.HandleFunc("GET /rooms/{id}", middleware.LoggingMiddleware(roomHandler.GetRoomByID))
 	http.HandleFunc("GET /rooms", middleware.LoggingMiddleware(roomHandler.ListRooms))
 	http.HandleFunc("PATCH /rooms/{id}", middleware.LoggingMiddleware(roomHandler.UpdateRoom))
 	http.HandleFunc("DELETE /rooms/{id}", middleware.LoggingMiddleware(roomHandler.DeleteRoom))
+
+	// Rotas de membros
+	http.HandleFunc("GET /rooms/{roomID}/members", middleware.LoggingMiddleware(memberHandler.ListMembers))
+	http.HandleFunc("POST /rooms/{roomID}/members", middleware.LoggingMiddleware(memberHandler.AddMember))
+	http.HandleFunc("PUT /rooms/{roomID}/members/{memberID}/role", middleware.LoggingMiddleware(memberHandler.UpdateRole))
+	http.HandleFunc("DELETE /rooms/{roomID}/members/{memberID}", middleware.LoggingMiddleware(memberHandler.RemoveMember))
 
 }
