0afe9756d5f153797ee678d0ec179ea16066e6c5
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2025-06-27 19:02:15 -0300

    feat: adding update and delete routes to user service

--- user-service/internal/user/delivery/rest/routes/user_router.go
+++ user-service/internal/user/delivery/rest/routes/user_router.go
@@ -12,4 +12,6 @@ func SetupUserRoutes(userHandler *handler.UserHandler) {
 	http.HandleFunc("GET /users", middleware.LoggingMiddleware(userHandler.GetAllUsers))
 	http.HandleFunc("GET /user/{id}", middleware.LoggingMiddleware(userHandler.GetByID))
 	http.HandleFunc("POST /user", middleware.LoggingMiddleware(userHandler.Register))
+	http.HandleFunc("PUT /user/{id}", middleware.LoggingMiddleware(userHandler.UpdateUser))
+	http.HandleFunc("DELETE /user/{id}", middleware.LoggingMiddleware(userHandler.DeleteUser))
 }
